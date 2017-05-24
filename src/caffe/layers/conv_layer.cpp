adba6a5ad7a11f57316e629bcd12c556bde5927c
YAO Matrix <matrix.yao@intel.com>
2017-05-24 17:27:26 +0800

    extend base conv data types to size_t to fit 3D conv requirements
    Signed-off-by: YAO Matrix <matrix.yao@intel.com>

--- src/caffe/layers/conv_layer.cpp
+++ src/caffe/layers/conv_layer.cpp
@@ -83,9 +83,9 @@ void ConvolutionLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       #pragma omp for
 #endif
       for (int n = 0; n < this->num_; ++n) {
-        this->forward_cpu_gemm(bottom_data + n*this->bottom_dim_,
+        this->forward_cpu_gemm(bottom_data + n * this->bottom_dim_,
                                weight,
-                               top_data + n*this->top_dim_);
+                               top_data + n * this->top_dim_);
         if (this->bias_term_) {
           const Dtype* bias = this->blobs_[1]->cpu_data();
           this->forward_cpu_bias(top_data + n * this->top_dim_, bias);
